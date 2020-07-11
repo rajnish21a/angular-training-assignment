22f569af23348ecc447aeea09791104c2df7d55c
rajnish21a <in.rajnish@live.in>
2020-07-11 06:32:20 +0530

    Route 1 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,24 +10,27 @@ import { SixthModule } from './sixth/sixth.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FifthComponent } from './fifth/fifth.component';
-import { SixthComponent } from './sixth/sixth.component';
+// import { FifthComponent } from './fifth/fifth.component';
+// import { SixthComponent } from './sixth/sixth.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/first', pathMatch:'full'},
   {path: 'first', loadChildren:()=>import('./first/first.module').then(m=>m.FirstModule).catch( err => console.log('Oh no!') )},
   {path: 'second', loadChildren:()=>import('./second/second.module').then((m)=>m.SecondModule).catch( err => console.log('Oh no!') )},
   {path: 'third', loadChildren:()=>import('./third/third.module').then(m=>m.ThirdModule).catch(err => console.log('Oh no!'))},
   {path: 'fourth', loadChildren:()=>import('./fourth/fourth.module').then(m=>m.FourthModule).catch(err => console.log('Oh no!'))},
   {path: 'fifth', loadChildren: ()=>import('./fifth/fifth.module').then(m=>m.FifthModule).catch(err => console.log('Oh no!'))},
-  {path: 'sixth', loadChildren:()=>import('./sixth/sixth.module').then(m=>m.SixthModule).catch(err=>console.log('Oh no!'))}
+  {path: 'sixth', loadChildren:()=>import('./sixth/sixth.module').then(m=>m.SixthModule).catch(err=>console.log('Oh no!'))},
+  {path: '', redirectTo: 'first', pathMatch:'full'},
+  {path: "**", component: PageNotFoundComponent	}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
